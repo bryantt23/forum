3bb1c776de8ecf2e74b7d7aa1f2618bf70cd764c
bryant tee <bryantt23@gmail.com>
2015-03-04 03:06:38 -0800

    finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   before_action :find_post, only:[:show, :edit, :destroy, :update]
-  before_action :authenticate!, except: [:show, :index]
   
   def index    
     @posts = Post.all.order("created_at DESC")
